ad2c8663d0662fd9a64d0b44c34393b9f95ff977
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-06-14 16:32:19 +0200

    add data on page

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ export default {
          API: 'http://127.0.0.1:8000/',
          API_PROJECTS: 'api/projects',
          img_path: 'storage/',
+         projects: null,
       }
    },
 
@@ -22,7 +23,8 @@ export default {
          axios
          .get(url)
          .then(resp => {
-            console.log(resp)
+            console.log(resp.data.projects)
+            this.projects = resp.data.projects
          })
       }
    },
@@ -37,6 +39,32 @@ export default {
 
 <template>
    <AppHeader></AppHeader>
+
+   <div class="container py-5">
+      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+         <div v-for="project in projects" class="col">
+            <div class="card h-100">
+               <img class="card-img-top" src="" alt="">
+               <div class="card-body">
+                  <h2>
+                     {{ project.title }}
+                  </h2>
+                  <p>
+                     {{ project.overview }}
+                  </p>
+               </div>
+               <div class="card-footer d-flex justify-content-between">
+                  <p>
+                     {{ project.link_code }}
+                  </p>
+                  <p>
+                     {{ project.link_code }}
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
 </template>
 
 <style lang="scss"></style>
