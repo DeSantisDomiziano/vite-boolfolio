3b5a760285b26677831a867ae4b9683f32e6832b
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-06-14 16:04:54 +0200

    bug cors

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <script>
 
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
+
 export default {
-    components: {
-        
-    }
+   components: {
+      AppHeader,
+   },
+
+   data() {
+      return {
+         loading: true,
+         API: 'http://127.0.0.1:8000/',
+         API_PROJECTS: 'api/projects',
+         img_path: 'storage/',
+      }
+   },
+
+   methods: {
+      get_projects(url) {
+         axios
+         .get(url)
+         .then(resp => {
+            console.log(resp)
+         })
+      }
+   },
+
+   mounted() {
+      console.log(this.API + this.API_PROJECTS);
+      this.get_projects(this.API + this.API_PROJECTS)
+   }
 }
+
 </script>
 
 <template>
-    
+   <AppHeader></AppHeader>
 </template>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
